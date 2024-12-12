2c870c6532dc0863970a8a3d1e7888254e2cbfe5
MattGuy00 <mattguy58@gmail.com>
2024-12-12 22:36:25 +1100

    Removed TODO.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,12 @@
 #include <SDL3/SDL_video.h>
 
 int main(int argc, const char** argv) {
-	// TODO: Add more error checking for argv
 	if (argc != 2) {
 		std::cout << "Usage: ./Chippy8 [path_to_rom]\n";
 		return 1;
 	}
 	
 	Chip8_Emu emulator {};
-
 	emulator.play(argv[1]);
 
     return 0;
